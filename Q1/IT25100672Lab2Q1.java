b36ebe4730571b4091c2d5675323e92d00f1c81a
Anushika2004 <it25100672@my.sliit.lk>
2026-09-16 11:41:24 +0530

    IT25100672Lab2Q1

---
+++ Q1/IT25100672Lab2Q1.java
@@ -0,0 +1,12 @@
+public class IT25100672Lab2Q1 {
+
+    public static void main(String[] args) {
+
+        double perimeter = 100;
+        double length = 100.0 / 3.5;
+        double width = (3.0 / 4.0) * length;
+
+        System.out.println("Length = " + length);
+        System.out.println("Width = " + width);
+    }
+}
