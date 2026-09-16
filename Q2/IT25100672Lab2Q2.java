b3ff13ea4819eaf872d8eae68405e73ca5baab80
Anushika2004 <it25100672@my.sliit.lk>
2026-09-16 11:42:26 +0530

    IT25100672Lab2Q2

---
+++ Q2/IT25100672Lab2Q2.java
@@ -0,0 +1,13 @@
+public class IT25100672Lab2Q2 {
+
+    public static void main(String[] args) {
+
+        double side = 10;
+        double squarePerimeter = 4 * side;
+
+        double pi = 22.0 / 7.0;
+        double radius = squarePerimeter / (2 * pi);
+
+        System.out.println("Radius = " + radius);
+    }
+}
